c44e5bf3e18bf32f5b3434a8c4d7e8d73624b501
LeinardB92 <71524074+LeinardB92@users.noreply.github.com>
2023-05-26 16:45:25 -0600

    Información del usuario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductPageComponent } from './products/pages/product-page/product-page.component';
 import { ProductsModule } from './products/products.module';
 
 
@@ -14,7 +14,8 @@ import { ProductsModule } from './products/products.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ProductsModule
+    ProductsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
